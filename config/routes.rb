822c50b91bff2ab5db9b377b78e6dc11c42695ca
Jason Beatty <jasondavidbeatty@gmail.com>
2015-04-06 22:50:55 -0400

    Create and update notes.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :sessions, only: :create
   delete 'logout' => 'sessions#destroy', as: :logout
   get 'login' => 'sessions#new', as: :login
+  resources :notes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
