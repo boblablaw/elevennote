5aa3b61b04cf741b7db9de965dc89de1a641a380
Jason Beatty <jasondavidbeatty@gmail.com>
2015-04-06 21:21:37 -0400

    Implement user sign up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :username, presence: true, uniqueness: true
   validates :password, length: { minimum: 8 }
+  has_many :notes
 end
