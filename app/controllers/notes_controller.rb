253b0d11ca7fc0e226cefe775471a01a4fa027a9
Jason Beatty <jasondavidbeatty@gmail.com>
2015-04-08 14:51:07 -0400

    Refactor NotesController

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,46 +1,30 @@
 class NotesController < ApplicationController
-  before_action :authorize_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authorize_user
+  before_action :find_note, only: [:show, :edit, :update, :destroy]
 
+  def show
+    render :edit
+  end
+  
   def new
     @note = Note.new
   end
 
-  def edit
-    @note = Note.find params[:id]
-  end
-
-  def show
-    @note = Note.find params[:id]
-  end
-
   def create
     @note = Note.new note_params
-    if @note.save
-      flash.now[:notice] = t('note.flash.create.success')
-      render :edit
-    else
-      flash.now[:alert] = t('note.flash.create.failure')
-      render :new
-    end
+    set_flash_for @note.save
+    render_or_redirect
   end
 
   def update
-    @note = Note.find params[:id]
-    if @note.update note_params
-      flash.now[:notice] = t('note.flash.update.success')
-    else
-      flash.now[:alert] = t('note.flash.update.failure')
-    end
-    render :edit
+    set_flash_for @note.update(note_params)
+    render_or_redirect
   end
 
   def destroy
     @note = Note.find params[:id]
-    if @note.destroy
-      redirect_to root_url, notice: t('note.flash.destroy.success')
-    else
-      redirect_to root_url, notice: t('note.flash.destroy.error')
-    end
+    set_flash_for @note.destroy
+    redirect_to new_note_path
   end
 
   private
@@ -48,4 +32,22 @@ class NotesController < ApplicationController
   def note_params
     params.require(:note).permit(:title, :body_html, :body_text)
   end
+
+  def find_note
+    @note = Note.find params[:id]
+  end
+
+  def set_flash_for(action_result)
+    if action_result
+      flash[:notice] = t("note.flash.#{action_name}.success")
+    else
+      flash.now[:alert] = t("note.flash.#{action_name}.failure")
+  end
+
+  def render_or_redirect
+    if @note.errors.any?
+      render :edit
+    else
+      redirect_to @note
+    end
 end
