66832ac00f6fdd93cd5e1e491270c49c49fea170
Jason Beatty <jasondavidbeatty@gmail.com>
2015-04-15 23:16:38 -0400

    use bootsy for text editor

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'bcrypt', '~> 3.1.7'
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
+gem 'bootsy'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -3,4 +3,8 @@ $(function() {
     ev.preventDefault();
     location.href = $(this).data('url');
   });
+  $(window).resize(function() {
+    $('#sidebar').height($(window).height() - 55);
+    $('.bootsy_text_area').height($(window).height() - 260);
+  }).trigger('resize');
 });
